5fa3baae0140a92def6446100c7349749f84dee9
carami <carami@nate.com>
2026-05-26 11:51:53 +0900

    update exam

--- springmvc/src/main/java/org/example/springmvc/controller/SessionController.java
+++ springmvc/src/main/java/org/example/springmvc/controller/SessionController.java
@@ -4,15 +4,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@SessionAttributes("visitCount")
 public class SessionController {
+    @ModelAttribute("visitCount")    //위에서 선언한 SessionAttribute를 초기화 하는 부분!!
+    public int getVisitCount() {
+        return 0;
+    }
+
+    @GetMapping("/visit")
+    public String visitPage(@ModelAttribute(name="visitCount")int visitCount, Model model) {
+        visitCount++;
+        model.addAttribute("visitCount", visitCount);
+
+        return "visit";
+    }
 //    상태정보로 유지하고 싶은 값을 받아오는 화면 요청
     @GetMapping("/sessionForm")
     public String sessionForm() {
@@ -45,4 +57,39 @@ public class SessionController {
         return "session_view";
 
     }
+
+    @GetMapping("/sessionDel")
+    public String sessionDel(@RequestParam("sessionKey") String sessionKey,HttpSession session) {
+        session.removeAttribute(sessionKey);
+
+        session.invalidate();   // 세션 전체 삭제!!
+        return "redirect:/sessionView";
+    }
+
+
+    @GetMapping("/listView")
+    public String listView(HttpSession session, Model model) {
+        Object attribute = session.getAttribute("login");
+//        로그인한 사용자에게만, welcome 페이지로 이동하고,
+        if(attribute!=null){
+            return "welcome";
+        }else {
+
+//        로그인하지 않은 사용자는 세션등록 페이지로 이동하게 코드를 만들어 볼까요?
+            return "redirect:/sessionForm";
+        }
+    }
+
+    @GetMapping("/listView2")
+    public String listView2(@SessionAttribute(name="login",required = false) Object attribute) {
+//        로그인한 사용자에게만, welcome 페이지로 이동하고,
+        if(attribute!=null){
+            return "welcome";
+        }else {
+
+//        로그인하지 않은 사용자는 세션등록 페이지로 이동하게 코드를 만들어 볼까요?
+            return "redirect:/sessionForm";
+        }
+    }
 }
+

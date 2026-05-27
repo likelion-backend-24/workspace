64fd15bb47297a8ebcdd02a2c6c5132f91ab5573
carami <carami@nate.com>
2026-05-27 14:47:42 +0900

    update exam

--- springjdbc/src/main/java/jdbc02/User.java
+++ springjdbc/src/main/java/jdbc02/User.java
@@ -1,18 +1,18 @@
-package jdbc01;
+package jdbc02;
 
 import lombok.*;
 
 import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    private Long userId;
-    private String userName;
+    private Long id;
+    private String name;
     private String email;
     private LocalDateTime createdAt;
-
 
 }

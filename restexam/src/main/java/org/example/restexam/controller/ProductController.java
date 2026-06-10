cd290e15a7498bdb1456d4946c74d71e6abb37e3
carami <carami@nate.com>
2026-06-10 12:56:27 +0900

    add rest api

---
+++ restexam/src/main/java/org/example/restexam/controller/ProductController.java
@@ -0,0 +1,40 @@
+package org.example.restexam.controller;
+
+import org.example.restexam.dto.ProductDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @GetMapping
+    public List<ProductDTO> getProducts(){
+        List<ProductDTO> products = Arrays.asList(
+                new ProductDTO(1L, "Laptop", 999),
+                new ProductDTO(2L, "Mouse", 29)
+        );
+
+        return products;
+    }
+
+    @PostMapping
+    public ProductDTO createProduct(@RequestBody ProductDTO productDTO){
+
+        return productDTO;
+    }
+
+    @PutMapping("/{id}")
+    public ProductDTO updateProduct(@RequestBody ProductDTO productDTO){
+
+        return productDTO;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProduct(){
+        return "삭제성공!!";
+    }
+
+}

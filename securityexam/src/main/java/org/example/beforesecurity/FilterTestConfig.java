9182e5e16a7c653b1a2d569d049c8f715efd54d2
carami <carami@nate.com>
2026-06-12 12:52:00 +0900

    add filter exam

---
+++ securityexam/src/main/java/org/example/beforesecurity/FilterTestConfig.java
@@ -0,0 +1,27 @@
+package org.example.beforesecurity;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterTestConfig {
+    @Bean
+    public FilterRegistrationBean<CaramiFilter> caramiFilter(){
+        FilterRegistrationBean<CaramiFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new CaramiFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
+
+
+    @Bean
+    public FilterRegistrationBean<FilterExam> filter(){
+        FilterRegistrationBean<FilterExam> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new FilterExam());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(2);
+        return registrationBean;
+    }
+}

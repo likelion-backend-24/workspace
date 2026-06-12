ec34ea0b34cb391759ce993022c2c5a0e0350f7b
carami <carami@nate.com>
2026-06-12 14:29:47 +0900

    update exam

--- securityexam/src/main/java/org/example/beforesecurity/UserFilter.java
+++ securityexam/src/main/java/org/example/beforesecurity/UserFilter.java
@@ -7,14 +7,28 @@ import java.io.IOException;
 public class UserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("UserFilter: doFilter 실행전:: Thread Name : "+Thread.currentThread().getName());
+        try {
+            System.out.println("UserFilter: doFilter 실행전:: Thread Name : " + Thread.currentThread().getName());
 
-        User user = new User("kang");
-        UserContext.setUser(user);
+//        User user = new User("kang");
+//        복잡한 로직이 실행되면서 원하는 값을 얻을거예요.
+            User user = extractUserFromRequest(request);
 
-        chain.doFilter(request,response);
 
-        System.out.println("UserFilter: doFilter 실행후:: Thread Name : "+Thread.currentThread().getName());
+            UserContext.setUser(user);
 
+            chain.doFilter(request, response);
+            System.out.println("UserFilter: doFilter 실행후:: Thread Name : "+Thread.currentThread().getName());
+
+        }finally {
+            UserContext.clear();
+        }
+    }
+
+    private User extractUserFromRequest(ServletRequest request){
+        //복잡한 일들을 하겠지만.. 우리는 간단하게..
+        String name = request.getParameter("name");
+
+        return new User(name);
     }
 }
